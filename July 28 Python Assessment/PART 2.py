b1ea9353e463862bff861c19a62af52f94b972cd
Kamalishree <113345175+Kamalishree@users.noreply.github.com>
2025-07-28 04:56:32 -0700

    Add files via upload

---
+++ July 28 Python Assessment/PART 2.py
@@ -0,0 +1,31 @@
+##class & inheritance
+## Person & Employee Class
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+class Employee(Person):
+    def __init__(self, name, age, employee_id, department):
+        super().__init__(name, age)
+        self.employee_id = employee_id
+        self.department = department
+
+    def display(self):
+        super().display()
+        print(f"Employee ID: {self.employee_id}, Department: {self.department}")
+
+#  Method Overriding Example
+
+
+class Vehicle:
+    def drive(self):
+        print("Driving a vehicle")
+
+class Car(Vehicle):
+    def drive(self):
+        print("Driving a car")
